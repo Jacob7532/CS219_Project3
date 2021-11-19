7f08e0d76dbd8e146deb80a4e5494ed7c55b80a5
Jacob <66694749+Jacob7532@users.noreply.github.com>
2021-11-18 17:26:07 -0800

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -3,46 +3,46 @@ import RPi.GPIO as GPIO
 import time 
 import math 
  
-buzzerPin= 11
+buzzerPin = 11
 buttonPin = 12
  
 def setup(): 
- global p  
- GPIO.setmode(GPIO.BOARD)
- GPIO.setup(buzzerPin, GPIO.OUT)
- GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-mode, and pull up to HIGH level, 3.3V 
- p = GPIO.PWM(buzzerPin, 1)  
- p.start(0); 
+    global p  
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(buzzerPin, GPIO.OUT)
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    p = GPIO.PWM(buzzerPin, 1)  
+    p.start(0); 
   
 def loop(): 
- while True: 
-  if GPIO.input(buttonPin)==GPIO.LOW: 
-   alertor() 
-   print ('alertor turned on >>> ') 
-  else : 
-   stopAlertor() 
-   print ('alertor turned off <<<') 
+    while True: 
+        if GPIO.input(buttonPin)==GPIO.LOW: 
+            alertor() 
+            print ('alertor turned on >>> ') 
+        else :
+            stopAlertor()
+            print ('alertor turned off <<<')
+            
 def alertor(): 
- p.start(50) 
- for x in range(0,361):
-  sinVal = math.sin(x * (math.pi / 180.0))
-  toneVal = 2000 + sinVal * 500
-  p.ChangeFrequency(toneVal)
-  time.sleep(0.001) 
+    p.start(50) 
+    for x in range(0,361):
+        sinVal = math.sin(x * (math.pi / 180.0))
+        toneVal = 2000 + sinVal * 500
+        p.ChangeFrequency(toneVal)
+        time.sleep(0.001) 
    
 def stopAlertor(): 
-  p.stop() 
+    p.stop() 
     
 def destroy(): 
- GPIO.output(buzzerPin, GPIO.LOW)
- GPIO.cleanup()
+    GPIO.output(buzzerPin, GPIO.LOW)
+    GPIO.cleanup()
  
 if __name__ == '__main__':
- print ('Program is starting...') 
- setup() 
- try: 
-  loop() 
- except KeyboardInterrupt:
-  destroy() 
+    print ('Program is starting...') 
+    setup() 
+    try: 
+        loop() 
+    except KeyboardInterrupt:
+        destroy() 
   #end of code from Freenove tutorial
