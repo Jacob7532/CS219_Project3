e903d54fede8e10defd649ba0b65f57daff08987
Jacob <66694749+Jacob7532@users.noreply.github.com>
2021-11-18 17:25:28 -0800

    Update doorbell.py

--- doorbell.py
+++ doorbell.py
@@ -5,27 +5,28 @@ buzzerPin = 11
 buttonPin = 12
  
 def setup():
- GPIO.setmode(GPIO.BOARD)        
- GPIO.setup(buzzerPin, GPIO.OUT)
- GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setmode(GPIO.BOARD)        
+    GPIO.setup(buzzerPin, GPIO.OUT)
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
  
 def loop(): 
- while True: 
-  if GPIO.input(buttonPin)==GPIO.LOW:
-   GPIO.output(buzzerPin,GPIO.HIGH)
-   print ('buzzer turned on >>>') 
-  else :
-   GPIO.output(buzzerPin,GPIO.LOW)
-   print ('buzzer turned off <<<') 
+    while True: 
+        if GPIO.input(buttonPin)==GPIO.LOW:
+            GPIO.output(buzzerPin,GPIO.HIGH)
+            print ('buzzer turned on >>>') 
+        else :
+            GPIO.output(buzzerPin,GPIO.LOW)
+            print ('buzzer turned off <<<') 
  
 def destroy(): 
- GPIO.cleanup()                     
+     GPIO.cleanup()                     
  
 if __name__ == '__main__':
     print ('Program is starting...') 
- setup() 
- try: 
-  loop() 
- except KeyboardInterrupt:
-  destroy() 
+    setup()
+    try: 
+        loop() 
+    except KeyboardInterrupt:
+        destroy() 
 #end of code from Freenove tutorial
+
